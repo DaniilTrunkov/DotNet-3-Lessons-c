50ff4b690e094646a3885865fc38c4b5791f95ec
DaniilTrunkov <75212904+DaniilTrunkov@users.noreply.github.com>
2020-11-29 19:34:31 +0300

    Task 29.11.2020

---
+++ Taks practicum/Taks practicum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taks_practicum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(() =>
+            {
+                int summ = 0;
+                for (int i = 1; i < 100 + 1; i++)
+                {
+                    summ = summ + i;
+                    
+                }
+                Console.WriteLine(summ);
+            });
+            t1.Start();
+            Console.ReadLine();
+        }
+    }
+}
